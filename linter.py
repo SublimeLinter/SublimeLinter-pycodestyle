bd9df8d97c7aba3eb2ef2fb09515591e73de7c30
herr kaste <herr.kaste@gmail.com>
2017-12-20 11:51:23 +0100

    Remove cruft from SL3

--- linter.py
+++ linter.py
@@ -20,7 +20,7 @@ class PYCODESTYLE(PythonLinter):
     """Provides an interface to the pep8 python module/script."""
 
     syntax = 'python'
-    cmd = ('pycodestyle@python', '*', '-')
+    cmd = ('pycodestyle', '*', '-')
     version_args = '--version'
     version_re = r'(?P<version>\d+\.\d+\.\d+)'
     version_requirement = '>= 1.4.6'
@@ -31,89 +31,3 @@ class PYCODESTYLE(PythonLinter):
         '--ignore=,': '',
         '--max-line-length=': None
     }
-    module = 'pycodestyle'
-    check_version = True
-
-    # Internal
-    report = None
-
-    def check(self, code, filename):
-        """Run pycodestyle on code and return the output."""
-        options = {
-            'reporter': self.get_report()
-        }
-
-        type_map = {
-            'select': [],
-            'ignore': [],
-            'max-line-length': 0,
-            'max-complexity': 0
-        }
-
-        self.build_options(options, type_map, transform=lambda s: s.replace('-', '_'))
-
-        final_options = options.copy()
-
-        # Try to read options from pycodestyle default configuration files (.pycodestyle, tox.ini).
-        # If present, they will override the ones defined by Sublime Linter's config.
-        try:
-            # `onError` will be called by `process_options` when no pycodestyle configuration file is found.
-            # Override needed to supress OptionParser.error() output in the default parser.
-            def onError(msg):
-                pass
-
-            from pycodestyle import process_options, get_parser
-            parser = get_parser()
-            parser.error = onError
-            pycodestyle_options, _ = process_options([os.curdir], True, True, parser=parser)
-
-            if pycodestyle_options.config:
-                pycodestyle_options = vars(pycodestyle_options)
-                pycodestyle_options.pop('reporter', None)
-                for opt_n, opt_v in pycodestyle_options.items():
-                    if isinstance(final_options.get(opt_n, None), list):
-                        final_options[opt_n] += opt_v
-                    else:
-                        final_options[opt_n] = opt_v
-        except SystemExit:
-            # Catch and ignore parser.error() when no config files are found.
-            pass
-
-        if persist.debug_mode():
-            persist.printf('{} ST options: {}'.format(self.name, options))
-            persist.printf('{} options: {}'.format(self.name, final_options))
-
-        checker = self.module.StyleGuide(**final_options)
-
-        return checker.input_file(
-            filename=os.path.basename(filename),
-            lines=code.splitlines(keepends=True)
-        )
-
-    def get_report(self):
-        """Return the Report class for use by flake8."""
-        if self.report is None:
-            from pycodestyle import StandardReport
-
-            class Report(StandardReport):
-                """Provides a report in the form of a single multiline string, without printing."""
-
-                def get_file_results(self):
-                    """Collect and return the results for this file."""
-                    self._deferred_print.sort()
-                    results = ''
-
-                    for line_number, offset, code, text, _ in self._deferred_print:
-                        results += '{path}:{row}:{col}: {code} {text}\n'.format_map({
-                            'path': self.filename,
-                            'row': self.line_offset + line_number,
-                            'col': offset + 1,
-                            'code': code,
-                            'text': text
-                        })
-
-                    return results
-
-            self.__class__.report = Report
-
-        return self.report
